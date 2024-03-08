f1f80e3cbcd3ed5d0ba9a4382b555b1d74f80b5d
Chirayu Intaratanoo <chirayu.intaratanoo@gmail.com>
2024-03-08 07:06:46 +0700

    Add Onboard blur+expand+opacity animation MyApp.swift

--- Foood/Foood Swift/MyApp.swift
+++ Foood/Foood Swift/MyApp.swift
@@ -2,9 +2,25 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+    @State private var isAnimated = false
     var body: some Scene {
         WindowGroup {
-            Onboarding()
+            ZStack {
+                Color.black
+                Onboarding()
+                    .frame(width: .infinity, height: .infinity)
+                    .scaleEffect(isAnimated ? 1 : 0.5)
+                    .animation(Animation.snappy(duration: 3), value: isAnimated)
+                    .opacity(isAnimated ? 1 : 0)
+                    .animation(Animation.snappy(duration: 3), value: isAnimated)
+                    .blur(radius: isAnimated ? 0 : 60)
+                    .animation(Animation.snappy(duration: 3), value: isAnimated)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            self.isAnimated.toggle()
+                        }
+                    }
+            }.ignoresSafeArea()
         }
     }
 }
